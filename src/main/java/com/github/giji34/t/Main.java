f1d0f258bac2a79ebe8677ca57db987c417438d4
kbinani <kbinani.bt@gmail.com>
2021-06-29 07:10:26 +0900

    feat: 暗視効果を付ける処理を復元

--- src/main/java/com/github/giji34/t/Main.java
+++ src/main/java/com/github/giji34/t/Main.java
@@ -527,8 +527,7 @@ public class Main extends JavaPlugin implements Listener {
         int amplifier = 0;
         boolean ambient = false;
         boolean particles = false;
-        player.removePotionEffect(PotionEffectType.NIGHT_VISION); //夏祭り用: 後で戻す
-        //effects.add(new PotionEffect(PotionEffectType.NIGHT_VISION, duration, amplifier, ambient, particles)); //夏祭り用: 後で戻す
+        effects.add(new PotionEffect(PotionEffectType.NIGHT_VISION, duration, amplifier, ambient, particles));
         effects.add(new PotionEffect(PotionEffectType.SATURATION, duration, amplifier, ambient, particles));
         effects.add(new PotionEffect(PotionEffectType.HEAL, duration, amplifier, ambient, particles));
         effects.add(new PotionEffect(PotionEffectType.WATER_BREATHING, duration, amplifier, ambient, particles));
